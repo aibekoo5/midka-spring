8ffb634e20b496004c68f719d762a9d21aa25793
AIBEK <aibek.kemel@narxoz.kz>
2025-12-03 16:00:00 +0500

    Old date commit

--- src/main/java/com/aibek/midkaspring/MidkaSpringApplication.java
+++ src/main/java/com/aibek/midkaspring/MidkaSpringApplication.java
@@ -3,6 +3,7 @@ package com.aibek.midkaspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//experiment
 @SpringBootApplication
 public class MidkaSpringApplication {
 
